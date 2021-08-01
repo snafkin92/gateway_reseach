b1dab9f1db7ff2f5562f022c88fa76455d59d62a
snafkin92 <87398136+snafkin92@users.noreply.github.com>
2021-08-01 21:54:53 +0900

    Add files via upload

---
+++ ftpTestUnix/FtpSampleLoad.java
@@ -0,0 +1,26 @@
+package ftpTestUnix;
+
+public class FtpSampleLoad {
+
+    public static void main(String[] args){
+        System.out.print("start: FtpSample\r\n");
+        FtpMng ftpClient = new FtpMng();
+
+        // １：接続
+        ftpClient.connect();
+
+        // ２：ログイン
+        ftpClient.login();
+
+        // ３：ダウンロード
+        //ftpClient.downLoad();
+
+        // ４：アップロード
+        ftpClient.upLoad();
+
+        // ５：切断
+        ftpClient.disConnect();
+
+        System.out.print("end: FtpSample\r\n");
+    }
+}
